a493792d87787b816eaa129e07d9afa4542bee08
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-07-12 15:14:13 +0200

    TEST: convert.tests.test_numpy: test for convert_file_extension function added

--- convert/tests/test_numpy.py
+++ convert/tests/test_numpy.py
@@ -75,17 +75,43 @@ def test_numpy_convert_file(shape, data_type, file_extension, other_file_extensi
     a = random_array(shape, data_type)
 
     # test save load and convert
+    FILENAME_FROM = pathlib.Path('test_file')
+    FILENAME_TO = pathlib.Path('test_file_converted')
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        # save base file
+        base_file_from = pathlib.Path(tmp_dir) / FILENAME_FROM
+        file = base_file_from.with_suffix(file_extension)
+        convert.numpy.save(file, a)
+        assert file.exists()
+        # convert file
+        base_file_to = pathlib.Path(tmp_dir) / FILENAME_TO
+        other_file = base_file_to.with_suffix(other_file_extension)
+        convert.numpy.convert_file(file, other_file)
+        assert file.exists()
+        assert other_file.exists()
+        # load converted file
+        b = convert.numpy.load(other_file)
+    assert np.allclose(a, b)
+
+
+@pytest.mark.parametrize('shape, data_type, file_extension, other_file_extension', test_numpy_convert_setups)
+def test_numpy_convert_file_extension(shape, data_type, file_extension, other_file_extension):
+    # generate test array
+    a = random_array(shape, data_type)
+
+    # test save load and convert
     FILENAME = pathlib.Path('test_file')
     with tempfile.TemporaryDirectory() as tmp_dir:
         # save base file
         base_file = pathlib.Path(tmp_dir) / FILENAME
         file = base_file.with_suffix(file_extension)
         convert.numpy.save(file, a)
+        assert file.exists()
         # convert file
-        other_file = base_file.with_suffix(other_file_extension)
-        convert.numpy.convert_file(file, other_file)
+        other_file = convert.numpy.convert_file_extension(file, other_file_extension)
         b = convert.numpy.load(other_file)
-        # test
         assert file.exists()
         assert other_file.exists()
+        # load converted file
+        b = convert.numpy.load(other_file)
     assert np.allclose(a, b)
