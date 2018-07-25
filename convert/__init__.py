a11be8d574ddc549296b4b23584d4aa0874978ef
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-07-25 12:12:05 +0200

    BLD: setup.py: python_requires >=3.6 added

--- convert/__init__.py
+++ convert/__init__.py
@@ -2,9 +2,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-import sys
-if sys.version_info.major < 3 or sys.version_info.minor < 6:
-    raise NotImplementedError('This modules needs at least Python 3.6.')
-del sys
-
 from convert.universal import load, save, convert_file, convert_file_extension, UnsupportedFileExtensionError, UnsupportedConversionError
