ec22802764f2b6c76c8ae0cff147dfe70307fd0e
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-04-05 10:20:01 +0200

    MAINT: setup.py: use 'AGPL' in license field instead of 'AGPLv3+'

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     url='https://github.com/jor-/io-convert',
     author='Joscha Reimer',
     author_email='jor@informatik.uni-kiel.de',
-    license='AGPLv3+',
+    license='AGPL',
 
     classifiers=[
         # Development Status
